3069bab3d65c4e563adab92e5697090a6b9d15fb
Ale <a@414c45.net>
2017-03-31 17:18:50 +0200

    Added basic test for City model

--- models/tests.py
+++ models/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from . import models
+from djgeojson.fields import PointField
 
-# Create your tests here.
+class cityTest(TestCase):
+
+    """ Create a city """
+    def create_city(self):
+        return models.City.objects.create(name="Eutropia", country="VA", latlon ="POINT (0.0 0.0)" )
+
+    """ Test city creation """
+    def test_city_creation(self):
+        city = self.create_city()
+        self.assertTrue(isinstance(city, models.City))
+        self.assertTrue(city.__str__, city.name)
