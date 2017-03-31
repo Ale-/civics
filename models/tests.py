83e2128d10b161362d14d587dd34d762238b400d
Ale <a@414c45.net>
2017-03-31 17:29:32 +0200

    Added basic test for Initiative model

--- models/tests.py
+++ models/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from . import models
 from djgeojson.fields import PointField
 
-class cityTest(TestCase):
+class CityTest(TestCase):
 
     """ Create a city """
     def create_city(self):
@@ -13,3 +13,29 @@ class cityTest(TestCase):
         city = self.create_city()
         self.assertTrue(isinstance(city, models.City))
         self.assertTrue(city.__str__, city.name)
+
+class InitiativeTest(TestCase):
+
+    """ Create a initiative """
+    def create_initiative(self):
+        city = CityTest.create_city(self)
+        return models.Initiative.objects.create(
+            name         = "Test initiative",
+            description  = "Lorem ipsum",
+            website      = "http://example.com",
+            twitter      = "http://twitter.com",
+            facebook     = "http://facebook.com",
+            email        = "test@example.com",
+            topic        = "OT",
+            space        = "OT",
+            agent        = "IM",
+            city         = city,
+            neighborhood = "Test neighborhood",
+            latlon       = city.latlon,
+        )
+
+    """ Test city creation """
+    def test_initiative_creation(self):
+        initiative = self.create_initiative()
+        self.assertTrue(isinstance(initiative, models.Initiative))
+        self.assertTrue(initiative.__str__, initiative.name)
