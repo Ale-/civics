c3e86660257783675a00d4527561aa1736002c37
Ale <a@414c45.net>
2017-04-25 11:08:00 +0200

    feat: Add fields to models/models.py
    Add fields to models related to metadata of the object (creation date, slug, manager),
    they are needed to data management in the backend (creation date), to generate urls related
    to modelforms (slug) and to give permissions over content (manager).

--- apps/models/models.py
+++ apps/models/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from . import geography_utils as geo
+from . import categories
 from djgeojson.fields import PointField
 from leaflet.forms.widgets import LeafletWidget
-from . import categories
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 #
 #  City
 #
+
 class City(models.Model):
+  """Model to represent City objects"""
+
   name     = models.CharField(_('Nombre de la ciudad'), max_length = 200, blank = False, null = True,
                               help_text = _('Especifica el nombre de la ciudad.'))
   country  = models.CharField(_('País'), max_length = 2, choices = geo.countries_as_tuple(), blank = False, null = True,
@@ -16,59 +21,76 @@ class City(models.Model):
   position = PointField(_("Ubicación"), blank=False, null=True,
                         help_text=_("Añade la ubicación de la ciudad."))
 
-  def __str__(self):
-    return self.name
-
   class Meta:
     verbose_name = _('Ciudad')
     verbose_name_plural = _('Ciudades')
 
+  def __str__(self):
+    """String representation of this model objects."""
+    return self.name
+
+
 #
 #  Initiative
 #
+
 class Initiative(models.Model):
+  """Model to represent Initiative objects"""
 
-  name         = models.CharField(_('Nombre de la iniciativa'), max_length=200, blank=False, null=True,
-                                  help_text=_('¿Cuál es el nombre de tu iniciativa?'))
-  description  = models.TextField(_('Descripción de la iniciativa'), blank=False, null=True,
-                                  help_text=_('Describe los objetivos y actividad de la iniciativa.'))
-  website      = models.URLField(_('Website'), blank=True, null=True,
-                                  help_text=_('Especifica opcionalmente una web para conocer mejor la iniciativa.'))
-  twitter      = models.CharField(_('Twitter'), blank=True, null=True, max_length = 128,
-                                  help_text=_('Si tienes una cuenta de Twitter, pon aquí el nombre de usuario.'))
-  facebook     = models.URLField(_('Facebook'), blank=True, null=True,
-                                 help_text=_('Si tienes un perfil de Facebook pon aquí su enlace.'))
-  email        = models.EmailField(_('Correo electrónico'), blank=True, null=True,
-                                   help_text=_('Especifica un correo de contacto para la iniciativa.'))
-  topic        = models.CharField(_('Tema'), blank=False, null=False, default='DC', max_length=2, choices = categories.TOPICS,
-                                   help_text=_('El tema de la iniciativa'))
-  space        = models.CharField(_('Tipo de espacio'), blank=False, null=False, default='CC', max_length=2, choices = categories.SPACES,
-                                  help_text=_('El tipo de espacio asociado a la iniciativa'))
-  agent        = models.CharField(_('tipo de agente'), blank=False, null=False, default='IM', max_length=2, choices = categories.AGENTS,
-                                  help_text=_('El tipo de agente involucrado en la iniciativa'))
-  city         = models.ForeignKey(City, verbose_name=_('Ciudad'), blank=False, null=True, on_delete=models.SET_NULL,
-                                   help_text=_('Ciudad donde se encuentra la iniciativa. Si no la encuentras en la lista puedes añadir una nueva.'))
-  address      = models.CharField(_('Dirección'), max_length = 200, blank=False, null=True,
-                                  help_text=_('Dirección de la iniciativa'))
-  district     = models.CharField(_('Distrito'), max_length = 200, blank=False, null=True,
-                                  help_text=_('¿En qué barrio de la ciudad se sitúa la iniciativa?'))
-  position     = PointField(_("Ubicación"), blank=False, null=True,
-                            help_text=_("Añade la ubicación de la iniciativa."))
-
-  def __str__(self):
-    return self.name
+  name          = models.CharField(_('Nombre de la iniciativa'), max_length=200, blank=False, null=True,
+                                   help_text=_('¿Cuál es el nombre de tu iniciativa?'))
+  slug          = models.SlugField(editable=False, blank=True)
+  creation_date = models.DateField(editable=False, default=timezone.now)
+  user          = models.ForeignKey(User, verbose_name=_('Gestor'), null=True, on_delete=models.SET_NULL)
+  description   = models.TextField(_('Descripción de la iniciativa'), blank=False, null=True,
+                                   help_text=_('Describe los objetivos y actividad de la iniciativa.'))
+  website       = models.URLField(_('Website'), blank=True, null=True,
+                                   help_text=_('Especifica opcionalmente una web para conocer mejor la iniciativa.'))
+  twitter       = models.CharField(_('Twitter'), blank=True, null=True, max_length = 128,
+                                   help_text=_('Si tienes una cuenta de Twitter, pon aquí el nombre de usuario.'))
+  facebook      = models.URLField(_('Facebook'), blank=True, null=True,
+                                  help_text=_('Si tienes un perfil de Facebook pon aquí su enlace.'))
+  email         = models.EmailField(_('Correo electrónico'), blank=True, null=True,
+                                    help_text=_('Especifica un correo de contacto para la iniciativa.'))
+  topic         = models.CharField(_('Tema'), blank=False, null=False, default='DC', max_length=2, choices = categories.TOPICS,
+                                    help_text=_('El tema de la iniciativa'))
+  space         = models.CharField(_('Tipo de espacio'), blank=False, null=False, default='CC', max_length=2, choices = categories.SPACES,
+                                   help_text=_('El tipo de espacio asociado a la iniciativa'))
+  agent         = models.CharField(_('tipo de agente'), blank=False, null=False, default='IM', max_length=2, choices = categories.AGENTS,
+                                   help_text=_('El tipo de agente involucrado en la iniciativa'))
+  city          = models.ForeignKey(City, verbose_name=_('Ciudad'), blank=False, null=True, on_delete=models.SET_NULL,
+                                    help_text=_('Ciudad donde se encuentra la iniciativa. Si no la encuentras en la lista puedes añadir una nueva.'))
+  address       = models.CharField(_('Dirección'), max_length = 200, blank=False, null=True,
+                                   help_text=_('Dirección de la iniciativa'))
+  district      = models.CharField(_('Distrito'), max_length = 200, blank=False, null=True,
+                                   help_text=_('¿En qué barrio de la ciudad se sitúa la iniciativa?'))
+  position      = PointField(_("Ubicación"), blank=False, null=True,
+                             help_text=_("Añade la ubicación de la iniciativa."))
 
   class Meta:
-    verbose_name = _('Iniciativa')
+    verbose_name        = _('Iniciativa')
     verbose_name_plural = _('Iniciativas')
+
+  def __str__(self):
+    """String representation of this model objects."""
+    return self.name
+
+  def save(self, *args, **kwargs):
+    """Populate automatically 'slug' field"""
+    self.slug = slugify(self.name)
+    super(Initiative, self).save(*args, **kwargs)
 
 #
 #  Event
 #
+
 class Event(models.Model):
+  """Model to represent Event objects"""
 
   initiative   = models.ForeignKey(Initiative, verbose_name=_("Iniciativa que organiza la actividad"), default=False, null=True,
                                    help_text=_('¿Qué iniciativa organiza el evento?'))
+  slug          = models.SlugField(editable=False, blank=True)
+  creation_date = models.DateField(editable=False, default=timezone.now)
   title        = models.CharField(_('Título del evento'), max_length = 200, blank=False, null=True,
                                   help_text=_('¿Cuál es el título del evento que quieres organiza?'))
   date         = models.DateField(_('Fecha del evento'),
@@ -86,9 +108,15 @@ class Event(models.Model):
   description  = models.TextField(_('Descripción de la iniciativa'), blank=False, null=True,
                                   help_text=_('Describe la actividad.'))
 
-  def __str__(self):
-    return self.title
-
   class Meta:
     verbose_name = _('Actividad')
     verbose_name_plural = _('Actividades')
+
+  def __str__(self):
+    """String representation of this model objects."""
+    return self.title
+
+  def save(self, *args, **kwargs):
+    """Populate automatically 'slug' field"""
+    self.slug = slugify(self.title)
+    super(Event, self).save(*args, **kwargs)
