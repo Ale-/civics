ac998bfbdac41b10f8f777a9d89e1c8975e3237e
Ale <a@414c45.net>
2017-04-25 16:55:41 +0200

    feat: Add base static folder
    Add base static folder of the project site, including a bower conf file
    to grab dependencies and a compass project to generate styles.

---
+++ static/civics/config.rb
@@ -0,0 +1,32 @@
+# To prevent problems of encoding when compiling:
+Encoding.default_external = "utf-8"
+
+# Additional plugins:
+require 'compass/import-once/activate'
+require 'sass-globbing'
+
+# Set abstractions as an import path to every partial
+add_import_path "sass/base/abstractions"
+
+# Set syntax to sass:
+preferred_syntax = :sass
+
+#
+# Paths and assets
+#
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "sass"
+images_dir      = "imf"
+fonts_dir       = "fonts"
+javascripts_dir = "js"
+relative_assets = true
+
+#
+# Output style
+#
+output_style  = (environment == :production) ? :compressed : :expanded
+line_comments = (environment == :production) ? false : true
+
+# To prevent warnings about deprecated stuff
+disable_warnings = true
