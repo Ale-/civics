8130bd52685fd1479a30e869034c1557b536a638
Ale <a@414c45.net>
2017-04-25 12:00:39 +0200

    feat: Add tags to include static assets to app/utils
    This tags are useful to create more reusable templates between projects.

---
+++ apps/utils/template_tags/utils.py
@@ -0,0 +1,13 @@
+from django import template
+from django.utils.translation import ugettext_lazy as _
+from civics.settings import STATIC_URL, PROJECT_STATIC_FOLDER
+
+register = template.Library()
+
+@register.simple_tag
+def css(file):
+    return  STATIC_DIR + PROJECT_STATIC_FOLDER + '/css/' + file
+
+@register.simple_tag
+def js(file):
+    return  STATIC_DIR + PROJECT_STATIC_FOLDER + '/js/' + file
--- apps/utils/tests.py
+++ apps/utils/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .template_tags import utils
+from civics import settings
 
-# Create your tests here.
+class FiltersTestCase(TestCase):
+
+    def CssTagFilterTest(self):
+        uri = 'test.css'
+        self.assertEqual( utils.css(context_url), settings.BASE_DIR + '/static/civics/css/' + uri )
+
+    def JsTagFilterTest(self):
+        uri = 'test.js'
+        self.assertEqual( utils.css(context_url), settings.BASE_DIR + '/static/civics/js/' + uri )
