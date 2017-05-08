89c070207183b7369b6e2f2c2347e42ebea1299b
Ale <a@414c45.net>
2017-05-08 13:01:10 +0100

    feat: Add login/registration templates, settings and base urls

--- civics/urls.py
+++ civics/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+from registration.forms import RegistrationFormUniqueEmail
+from registration.backends.default.views import RegistrationView
+from apps.models.views import InitiativeCreate
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='pages/front.html'), name="front"),
     url(r'^', include('apps.models.urls', namespace='modelforms')),
+
+    # Registration URLs
+    url(r'^registrate/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name="registration_register"),
+    url(r'', include('registration.backends.default.urls')),
 ]
