cea80b5ed34a9e8ea4244f9bb6c7bcbf212d3f5d
Ale <a@414c45.net>
2017-06-02 16:46:22 +0100

    fix: Add static and media folders to urlpatterns in debug mode

--- civics/urls.py
+++ civics/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
@@ -21,3 +23,7 @@ urlpatterns = [
     url(r'^api/', include('apps.api.urls', namespace="api")),
     url(r'^', include('apps.users.urls', namespace="users")),
 ]
+
+if settings.DEBUG == True:
+  urlpatterns += static( settings.STATIC_URL, document_root = settings.STATIC_ROOT )
+  urlpatterns += static( settings.MEDIA_URL,  document_root = settings.MEDIA_ROOT )
