889b97b0e0b745b17b84c14c14ec5483bdc64194
Ale <a@414c45.net>
2017-06-02 11:35:23 +0100

    feat: Update registration workflow
    Update registration workflow to redirect user after activation to
    the the Initiative creation modelform.

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,8 +2,43 @@ from django.shortcuts import render
 from django.views import View
 from apps.models.models import City
 from apps.models import categories
+from registration.views import ActivationView as BaseActivationView
+from registration.models import RegistrationProfile
+from registration import signals
+from django.contrib.sites.shortcuts import get_current_site
 
 class Dashboard(View):
 
     def get(self, request):
         return render(request, 'users/dashboard.html', locals())
+
+
+# Custom activation view that redirects user to Create Initiative form
+
+class ActivationView(BaseActivationView):
+
+    registration_profile = RegistrationProfile
+
+    def activate(self, *args, **kwargs):
+        """
+        Given an an activation key, look up and activate the user
+        account corresponding to that key (if possible).
+
+        After successful activation, the signal
+        ``registration.signals.user_activated`` will be sent, with the
+        newly activated ``User`` as the keyword argument ``user`` and
+        the class of this backend as the sender.
+
+        """
+        activation_key = kwargs.get('activation_key', '')
+        site = get_current_site(self.request)
+        activated_user = (self.registration_profile.objects
+                          .activate_user(activation_key, site))
+        if activated_user:
+            signals.user_activated.send(sender=self.__class__,
+                                        user=activated_user,
+                                        request=self.request)
+        return activated_user
+
+    def get_success_url(self, user):
+        return ('modelforms:create_initiative', (), {})
--- civics/urls.py
+++ civics/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 from registration.forms import RegistrationFormUniqueEmail
 from registration.backends.default.views import RegistrationView
-from apps.models.views import InitiativeCreate
+from apps.users.views import ActivationView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^acerca$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
     # Registration URLs
+    url(r'^activate/(?P<activation_key>\w+)/$', ActivationView.as_view(), name='registration_activate'),
     url(r'^registrate/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name="registration_register"),
     url(r'', include('registration.backends.default.urls')),
     url(r'^api/', include('apps.api.urls', namespace="api")),
