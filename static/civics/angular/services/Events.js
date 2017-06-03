0ad46078179a88a454107b1e1a99e6ef1de60a6f
Ale <a@414c45.net>
2017-06-03 10:27:39 +0100

    fix: Homegeneize event fetching from API

--- static/civics/angular/services/Events.js
+++ static/civics/angular/services/Events.js
@@ -6,25 +6,27 @@ angular.module('civics.events_service', [])
 
         setup: function(){
             return $http.get('/api/events?city=all&topics=all&categories=all&agents=all').
-            then( function(response){
-                if(typeof response.data === 'object') {
+            then( angular.bind(this, function(response){
+                if(Array.isArray(response.data)){
                     response.data.forEach(function(marker){
                         marker.icon = {
                             'type' : 'div',
                             'iconSize'    : [40, 60],
                             'iconAnchor'  : [20, 60],
                             'popupAnchor' : [0, -30],
-                            'message'     : marker.name,
-                            'html'        : "<div class='event-marker'>" +
-                                            "<i class='outer topic-" + marker.topic + " agent-" + marker.agent + "'></i>" +
-                                                "<i class='inner space-" + marker.space + "'></i>" +
-                                             "</div>",
+                            'message'     : marker.title,
+                            'html'        : "<div class='initiative-marker'>" +
+                                            "<i class='outer icon-topic-" + marker.topic + " icon-agent-" + marker.agent + "'></i>" +
+                                                "<i class='inner icon-category-" + marker.category + "'></i>" +
+                                            "</div>",
                         }
                     });
-                    return response.data;
-                } else {
-                    return [];
+                    this.markers = response.data;
                 }
+                return this.markers;
+
+            }), function(error_response){
+                console.log(error_response);
             });
         },
     }
