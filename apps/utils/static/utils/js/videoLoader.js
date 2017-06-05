2b660e80edb7ee61b55b6fac897fe2b15d9c2263
Ale <a@414c45.net>
2017-06-05 18:46:25 +0100

    fix: Update videoloader widget to visualize video when editing models7

--- apps/utils/static/utils/js/videoLoader.js
+++ apps/utils/static/utils/js/videoLoader.js
@@ -9,6 +9,10 @@
     widgets.forEach(function(widget){
         var input       = widget.querySelector('input');
         var placeholder = widget.querySelector('.placeholder');
+        if(input.value){
+            html = createIframe(input.value, placeholder.dataset.width, placeholder.dataset.height);
+            widget.querySelector('.placeholder').innerHTML = html;
+        }
         widget.querySelector('.video-widget__submit').addEventListener('click', function(){
             html = createIframe(input.value, placeholder.dataset.width, placeholder.dataset.height);
             widget.querySelector('.placeholder').innerHTML = html;
