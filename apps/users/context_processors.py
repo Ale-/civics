0c810c9959a17d1b7a668037095adabc31a18fee
Ale <a@414c45.net>
2017-06-06 15:46:40 +0100

    feat: Update 'hello' context processor

--- apps/users/context_processors.py
+++ apps/users/context_processors.py
@@ -8,14 +8,13 @@ def current_user_hello(request):
 
     current_user = request.user
 
-    if current_user.is_staff:
-        current_user_hello = "<a href='/admin'>admin</a>"
-    elif current_user.is_authenticated:
+    if current_user.is_authenticated:
         current_user_initiative = Initiative.objects.filter(user=request.user).first()
+        current_user_hello = "Hola, "
         if current_user_initiative:
-            current_user_hello = "Hola, <a href='" + reverse('users:dashboard') + "'>" + current_user_initiative.name + "</a>"
+            current_user_hello += "<a href='" + reverse('users:dashboard') + "'>" + current_user_initiative.name + "</a>"
         else:
-            current_user_hello = "Bienvenido! <a href='/'>¿Creaste tu iniciativa</a>?"
+            current_user_hello = "Hola, <a href='" + reverse('modelforms:create_initiative') + "'>¿creaste tu iniciativa</a>?"
     else:
         current_user_hello = None
 
