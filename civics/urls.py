104b6a1164cc4fbbaed8040f6d823d292747192c
Ale <a@414c45.net>
2017-06-07 12:27:32 +0100

    feat: Add password recovery workflow
    - Add password recovery templates
    - Update field template to safe embed help texts as password reset
    template needs it.
    - Add password recovery urls.
    - Add needed styles.

--- civics/urls.py
+++ civics/urls.py
@@ -19,6 +19,10 @@ urlpatterns = [
     # Registration URLs
     url(r'^activate/(?P<activation_key>\w+)/$', ActivationView.as_view(), name='registration_activate'),
     url(r'^registrate/$', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name="registration_register"),
+    url(r'^me-olvide-el-pass', auth_views.password_reset, name='password_reset'),
+    url(r'^confirma-pass/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name="password_reset_confirm"),
+    url(r'^pass-cambiado/$', auth_views.password_reset_complete, name="password_reset_complete"),
+    url(r'^pass/ok/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'', include('registration.backends.default.urls')),
     url(r'^api/', include('apps.api.urls', namespace="api")),
     url(r'^', include('apps.users.urls', namespace="users")),
