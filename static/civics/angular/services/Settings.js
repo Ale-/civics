0707a1f560a75dbd9c18df552815f99e59161123
Ale <a@414c45.net>
2017-06-09 12:48:22 +0100

    fix: Change default order of maps in angular app

--- static/civics/angular/services/Settings.js
+++ static/civics/angular/services/Settings.js
@@ -19,6 +19,22 @@ angular.module('civics.settings', [])
 
       this.map_layers = {
           baselayers: {
+              civics: {
+                name: 'civics',
+                url: 'https://api.mapbox.com/styles/v1/civics/cir1q2kud001icmm9tmh4s9lt/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2l2aWNzIiwiYSI6ImNpcXpmZ2toZTAwNmFpOG1nc2swdG5kZ28ifQ.P6-IjrcLcdnPqQvkn0GWKQ',
+                type: 'xyz',
+              },
+              toner : {
+                name         : 'Toner',
+                url          : 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
+                layerOptions : {
+                  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
+                  under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. \
+                  Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, \
+                  under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+                },
+                type: 'xyz',
+              },
               mapnik : {
                   name         : 'Open Street Map',
                   url          : 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
@@ -26,22 +42,6 @@ angular.module('civics.settings', [])
                        attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   },
                   type : 'xyz',
-              },
-              civics: {
-                   name: 'civics',
-                   url: 'https://api.mapbox.com/styles/v1/civics/cir1q2kud001icmm9tmh4s9lt/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2l2aWNzIiwiYSI6ImNpcXpmZ2toZTAwNmFpOG1nc2swdG5kZ28ifQ.P6-IjrcLcdnPqQvkn0GWKQ',
-                   type: 'xyz',
-              },
-              toner : {
-                  name         : 'Toner',
-                  url          : 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
-                  layerOptions : {
-                       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
-                                    under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. \
-                                    Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, \
-                                    under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-                  },
-                  type: 'xyz',
               },
           },
           overlays: {
