d1a4de6a4f9727f683722a2fb19d46b38694be89
ale <a@414c45.net>
2017-09-07 09:37:22 +0200

    feat: Add context processor to check debug flags in templates

--- apps/utils/context_processors.py
+++ apps/utils/context_processors.py
@@ -1,9 +1,18 @@
-from civics.settings import DOCUMENT_TITLE, DOCUMENT_DESCRIPTION
+from django.conf import settings
 
 def site_info_processor(request):
     """Injects into global context information about the site"""
 
-    document_title       = DOCUMENT_TITLE
-    document_description = DOCUMENT_DESCRIPTION
+    document_title       = settings.DOCUMENT_TITLE
+    document_description = settings.DOCUMENT_DESCRIPTION
+
+    return locals()
+
+def debug_processor(request):
+    """Injects debug flag into context"""
+
+    debug    = settings.DEBUG
+    debug_js = settings.DEBUG_JS
+    print(debug_js)
 
     return locals()
--- civics/settings.py
+++ civics/settings.py
@@ -92,6 +92,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.utils.context_processors.site_info_processor',
+                'apps.utils.context_processors.debug_processor',
                 'apps.users.context_processors.current_user_hello',
             ],
         },
