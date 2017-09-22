27a168a0112fda410cc38fe513447b37e83ced6b
ale <a@414c45.net>
2017-09-22 19:41:27 +0100

    feat: Update angular translations

--- static/civics/angular/i18n.js
+++ static/civics/angular/i18n.js
@@ -57,6 +57,7 @@ angular.module('civics.i18n', [])
           'Contacta con Civics' : 'Contato Civismo',
           //directives/time-filter.html
           'Todas' : 'Todos',
+          'Vigentes' : 'Em vigor',
           'Hoy' : 'Hoje',
           'Mañana' : 'Amanhã',
           'Próximos 7 días' : 'Próximos 7 dias',
@@ -105,6 +106,21 @@ angular.module('civics.i18n', [])
           'Fiesta / Concierto' : 'Festa / Concerto',
           'Publicación' : 'Postagem',
           'Taller' : 'Oficina',
+          // help_texts
+          'Puedes usar este enlace para compartir la vista actual del mapa' : 'Você pode usar esse link para compartilhar a visualização atual do mapa',
+          'Ver contenido en formato lista' : 'Exibir conteúdo no formato de lista',
+          'Ver contenido destacado' : 'Exibir conteúdo destacado',
+          'Descargar vista actual en formato Excel' : 'Baixar vista atual no formato Excel',
+          'Generar un enlace para compartir la vista actual' : 'Gerar um link para compartilhar a visualização atual',
+          'Cambiar la capa base del mapa' : 'Alterar mapa base',
+          'Volver a la vista inicial de todo el contenido' : 'Voltar para a visão inicial de todo o conteúdo',
+          'Localízate en el mapa' : 'Localize-se no mapa',
+          'En esta esquina tienes distintas acciones que puedes realizar sobre el contenido mostrado:' : 'Neste canto, você possui diferentes ações que você pode executar no conteúdo mostrado:',
+          'Ver contenido en formato mapa' : 'Exibir conteúdo no formato de mapa',
+          'Puedes usar este buscador para encontrar iniciativas por su nombre. Se autocompleta a partir de 3 caracteres insertados' : 'Você pode usar este mecanismo de busca para encontrar iniciativas pelo nome. Autocompletar de 3 caracteres inseridos',
+          'Usa estos botones para encontrarnos en otras plataformas o contactar con nosotros' : 'Use estes botões para nos encontrar em outras plataformas ou contate-nos',
+          'Aquí puedes filtrar los eventos en función de su fecha' : 'Aqui você pode filtrar os eventos de acordo com sua data',
+          'Esta es la leyenda del mapa. Puedes pulsar en las ciudades y distintas categorías para filtrar los contenidos visibles según tus intereses' : 'Esta é a lenda do mapa. Você pode clicar nas cidades e categorias diferentes para filtrar os conteúdos visíveis de acordo com seus interesses',
         }
     };
 
