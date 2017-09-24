a9bff7445ad3fa466323045d37a5bd5542e374d9
ale <a@414c45.net>
2017-09-24 19:59:53 +0100

    feat: Add logout redirect page. Update login redirect

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,11 @@
+# django
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
+# project
 from apps.models.models import Initiative
 from . import views
 
 urlpatterns = [
     url(r'^mi-perfil$', views.Dashboard.as_view(), name='dashboard'),
+    url(r'^gracias$',   TemplateView.as_view(template_name='pages/goodbay.html'), name='thanks'),
 ]
